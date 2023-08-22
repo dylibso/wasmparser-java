d3fc54a8db2af13d13e3d992139463f752fb1e1e
Benjamin Eckel <bhelx@simst.im>
2023-08-22 18:42:27 -0500

    dont need this type anymore

--- src/main/java/com/dylibso/wasm/types/InstructionSignature.java
+++
@@ -1,9 +0,0 @@
-package com.dylibso.wasm.types;
-
-public class InstructionSignature {
-    private WasmEncoding[] operands;
-
-    public InstructionSignature(WasmEncoding[] operands) {
-        this.operands = operands;
-    }
-}
